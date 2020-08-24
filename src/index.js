61296da8dc6a777d242bcb64f67e3a69db9c38d1
jung-hye <hoho4247@gmail.com>
2020-08-24 15:09:18 +0900

    Vanilla-Redux 사용

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+const countModifier = (count = 0, action) => {
+  return count;
+};
+
+const countStore = createStore(countModifier);
+console.log(countStore); 
+
+countStore.dispatch({type: 'hello'});
