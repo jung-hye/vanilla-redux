384fe4b648dab1a6c6405c1f1b205ab74ee5d8d4
은정혜 <junghye.eun@wadiz.kr>
2020-08-24 15:41:25 +0900

    1차 vanilla-redux counter 완성

---
+++ src/dist/index.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _redux = require("redux");
+
+var add = document.getElementById('add');
+var minus = document.getElementById('minus');
+var number = document.querySelector('span'); // 상태 초기값 및 액션 타입에 따른 할 일 설정(reducer)
+
+var countModifier = function countModifier() {
+  var count = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var action = arguments.length > 1 ? arguments[1] : undefined;
+
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    // 최종 상태를 리턴!
+    return count;
+  }
+}; // 데이터 저장소 만들고, 액션 받을 함수 지정
+
+
+var countStore = (0, _redux.createStore)(countModifier); // 상태값 변경 시 해야할 일 지정
+
+var onChange = function onChange() {
+  number.innerText = countStore.getState();
+}; // modify 액션이 일어나는 dom 설정(type 프로퍼티로 이루어진 객체여야 함)
+
+
+add.addEventListener("click", function () {
+  countStore.dispatch({
+    type: 'ADD'
+  });
+});
+minus.addEventListener("click", function () {
+  countStore.dispatch({
+    type: 'MINUS'
+  });
+}); // store의 변화 감지 함수 부착
+
+countStore.subscribe(onChange);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+// 상태 초기값 및 액션 타입에 따른 할 일 설정(reducer)
 const countModifier = (count = 0, action) => {
-  return count;
+  if(action.type === 'ADD') {
+    return count + 1;
+  }else if(action.type === "MINUS") {
+    return count  - 1;
+  }else {
+    // 최종 상태를 리턴!
+    return count;
+  }
 };
 
+// 데이터 저장소 만들고, 액션 받을 함수 지정
 const countStore = createStore(countModifier);
-console.log(countStore); 
 
-countStore.dispatch({type: 'hello'});
+// 상태값 변경 시 해야할 일 지정
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+// modify 액션이 일어나는 dom 설정(type 프로퍼티로 이루어진 객체여야 함)
+add.addEventListener("click", () => {countStore.dispatch({type: 'ADD'})});
+minus.addEventListener("click", () => {countStore.dispatch({type: 'MINUS'})});
+
+// store의 변화 감지 함수 부착
+countStore.subscribe(onChange);
