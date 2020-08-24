a9506486c9472aeed9b4e0cf4f931bf672e2d2e9
은정혜 <junghye.eun@wadiz.kr>
2020-08-24 18:38:53 +0900

    vanilla-redux To Do List 리팩토링

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -2,31 +2,26 @@
 
 var _redux = require("redux");
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
-
-var form = document.querySelector('form');
 var input = document.querySelector('input');
+var btn = document.querySelector('button');
 var ul = document.querySelector('ul');
-var ADD_TODO = "ADD_TODO";
-var DELETE_TODO = "DELETE_TODO";
 
-var reducer = function reducer() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-  var action = arguments.length > 1 ? arguments[1] : undefined;
+var ADD_TODO = function ADD_TODO() {
+  return {
+    type: 'ADD_TODO'
+  };
+};
 
+var DELETE_TODO = function DELETE_TODO() {
+  return {
+    type: 'DELETE_TODO'
+  };
+};
+
+var reducer = function reducer(state, action) {
   switch (action.type) {
     case ADD_TODO:
-      // NEVER MUTATE STATE
-      return [].concat(_toConsumableArray(state), [{
-        text: action.text,
-        id: Date.now()
-      }]);
+      return [];
 
     case DELETE_TODO:
       return [];
@@ -37,18 +32,14 @@ var reducer = function reducer() {
 };
 
 var store = (0, _redux.createStore)(reducer);
-store.subscribe(function () {
-  console.log(store.getState());
-});
 
-var onSubmit = function onSubmit(e) {
-  e.preventDefault();
-  var toDo = input.value;
-  input.value = "";
-  store.dispatch({
-    type: ADD_TODO,
-    text: toDo
-  });
+var onChange = function onChange(e) {
+  var value = e.target.value;
 };
 
-form.addEventListener('submit', onSubmit);
+var onClick = function onClick() {
+  store.dispatch(ADD_TODO);
+};
+
+input.addEventListener('change', onChange);
+btn.addEventListener('click', onClick);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const ul = document.querySelector('ul');
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
 const reducer = (state = [], action) => {
   switch(action.type) {
     case ADD_TODO:
@@ -14,24 +27,44 @@ const reducer = (state = [], action) => {
       return [...state, { text: action.text, id: Date.now() }];
 
     case DELETE_TODO:
-      return [];
-
+      return state.filter(toDo => toDo.id !== action.id)
+ 
     default:
-      return state;
+      return state; 
   }
 };
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
+const dispatchAddTodo = text => {
+  store.dispatch(addToDo(text));
+}
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+const paintTodos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = "DEL";
+    btn.addEventListener('click', dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintTodos);
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO , text: toDo});
+  dispatchAddTodo(toDo);
 }
 
 form.addEventListener('submit', onSubmit);
